5f235a13756df8d114e64c35bb265f831bd69c7e
Lucaslofe <lucasf5@al.insper.edu.br>
2022-05-04 15:13:40 -0300

    Comecei a criar o dicionário

---
+++ Ep2_jogo.py
@@ -0,0 +1,24 @@
+paises = {}
+paises["América do Sul"] = {
+    "Brasil": 
+        [
+            "Verde, Amarelo, Azul e Branco",
+            "Brasília", 
+            "214 milhões", 
+            "8.516.000 km²", 
+            "América do Sul"
+        ],
+    "Chile":
+        [
+
+        ],
+            }
+
+paises["América Central"] = {}
+paises["América do Norte"] = {}
+paises["Europa"] = {}
+paises["África"] = {}
+paises["Ásia"] = {}
+paises["Oceânia"] = {}
+paises["Antártica"] = {}
+
