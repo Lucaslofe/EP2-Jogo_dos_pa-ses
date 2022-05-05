fdddd6f0e0be770c5dfb09c61eeee11f46602fe4
Lucaslofe <lucasf5@al.insper.edu.br>
2022-05-04 21:51:48 -0300

    adicionei todos os países da américa do sul

--- Ep2_jogo.py
+++ Ep2_jogo.py
@@ -10,8 +10,95 @@ paises["América do Sul"] = {
         ],
     "Chile":
         [
-
+            "Azul, Branco e Vermelho",
+            "Santiago",
+            "19 Milhões",
+            "756.950 km²",
+            "América do Sul"
         ],
+    "Argentina":
+        [
+            "Azul e Branco",
+            "Buenos Aires",
+            "45 Milhões",
+            "2.780.000 km²",
+            "América do Sul"
+        ],
+    "Equador":
+        [
+            "Amarelo, Azul e Vermelho",
+            "Quito",
+            "19 Milhões",
+            "283.560 km²",
+            "América do Sul"
+        ],
+    "Bolívia":
+        [
+            "Vermelho, Amarelo e Verde",
+            "La Paz",
+            "11 Milhões",
+            "1.099.000 km²",
+            "América do Sul"
+        ],
+    "Colômbia":
+        [
+            "Amarelo, Azul e Vermelho",
+            "Bogotá",
+            "50 Milhões",
+            "1.142.000 km²",
+            "América do Sul"
+        ],
+    "Venezuela":
+        [
+            "Amarelo, Azul, Branco e Vermelho",
+            "Carcas",
+            "28 Milhões",
+            "916.445 km²",
+            "América do Sul"
+        ],
+    "Uruguai":
+        [
+            "Branco e Azul",
+            "Montevideo",
+            "3 Milhões",
+            "176.215 km²",
+            "América do Sul"
+        ],
+    "Paraguai":
+        [
+            "Vermelho, Branco e Azul",
+            "Assunção",
+            "7 Milhões",
+            "406.752 km²",
+            "América do Sul"
+        ],
+    "Guiana":
+        [
+            "Vermelha, Preta, Amarela, Branca e Verde",
+            "Georgetown",
+            "786 mil",
+            "214.969 km²",
+            "América do Sul"
+        ],
+    "Suriname":
+        [
+            "Verde, Branco e Amarelo",
+            "Paramaribo",
+            "558 mil",
+            "163.820 km²",
+            "América do Sul"
+        ],
+    "Guiana Francesa":
+        [
+            "Azul, Branca e Vermelha",
+            "Caiena",
+            "294 mil",
+            "83.846 km²",
+            "América do Sul"
+        ]
+
+
+    
             }
 
 paises["América Central"] = {}
